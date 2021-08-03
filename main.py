052a3b0e1db53e006e0fcc0ffcd7e986146813bc
WilsonVinson <cprewilsonvinson@gmail.com>
2021-08-03 13:45:39 +0800

    main

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import panda3d
 from panda3d.core import loadPrcFile
 loadPrcFile('config/config.prc')
+
+from panda3d.core import loadPrcFileData
 
 from direct.showbase.ShowBase import ShowBase
 
@@ -7,6 +10,15 @@ class MyGame(ShowBase):
 
     def __init__(self):
         super().__init__(self)
+
+        box = self.loader.loadModel('models/box')
+        box.setPos(0,10,0)
+        box.reparentTo(self.render)
+
+        panda = self.loader.loadModel('models/panda')
+        panda.setPos(-3,10,0)
+        panda.setScale(0.1,0.1,0.1)
+        panda.reparentTo(self.render)
 
 game = MyGame()
 
