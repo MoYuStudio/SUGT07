a580a13d71b206a334d22940bd41ebbddef6ff5d
WilsonVinson <cprewilsonvinson@gmail.com>
2021-08-09 11:29:22 +0800

    main

--- moyu_engine/config/graphics.py
+++ moyu_engine/config/graphics.py
@@ -1,8 +1,8 @@
 
 import pyglet
 
-tl1 = pyglet.image.load('moyu_engine/assets/graphics/tileland/tl1.png').convert_alpha()
-tl6 = pyglet.image.load('moyu_engine/assets/graphics/tileland/tl6.png').convert_alpha()
-tl11 = pyglet.image.load('moyu_engine/assets/graphics/tileland/tl11.png').convert_alpha()
-tl16 = pyglet.image.load('moyu_engine/assets/graphics/tileland/tl16.png').convert_alpha()
-tl21 = pyglet.image.load('moyu_engine/assets/graphics/tileland/tl21.png').convert_alpha()
+tl1 = pyglet.image.load('moyu_engine/assets/graphics/tileland/tl1.png')
+tl6 = pyglet.image.load('moyu_engine/assets/graphics/tileland/tl6.png')
+tl11 = pyglet.image.load('moyu_engine/assets/graphics/tileland/tl11.png')
+tl16 = pyglet.image.load('moyu_engine/assets/graphics/tileland/tl16.png')
+tl21 = pyglet.image.load('moyu_engine/assets/graphics/tileland/tl21.png')
