5e2c5e69ac13503c3d9973009a279d938a13ebe9
WilsonVinson <cprewilsonvinson@gmail.com>
2021-08-09 10:46:52 +0800

    main

---
+++ moyu_engine/config/setup.py
@@ -0,0 +1,46 @@
+
+import sys
+import pygame
+from pygame.locals import *
+
+import constants as C
+import graphics as G
+import font as F
+
+import components.tilemap_manager
+import components.tilebutton
+import components.window_move
+
+import window.main.main_window
+
+def run():
+    init()
+    gameloop()
+
+def init():
+
+    pygame.init()
+
+    C.mainwindow = pygame.display.set_mode((1200,600))
+    C.window_title = pygame.display.set_caption('TinyLand 弹丸之地')
+    pygame.display.set_icon(G.tl6)
+    C.clock = pygame.time.Clock()
+    pygame.display.flip()
+
+    return C.mainwindow,C.window_title,C.clock
+
+def gameloop():
+
+    components.tilemap_manager.tilemap_builder()
+
+    while True:
+
+        window.main.main_window.display()
+        
+        C.tile_size = 64*C.tile_level
+
+        window.main.main_window.event()
+
+        C.clock.tick(60)
+
+run()
