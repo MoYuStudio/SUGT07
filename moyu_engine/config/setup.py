cd99808eaa3aa7294e243ccff6f04064445261d9
WilsonVinson <cprewilsonvinson@gmail.com>
2021-08-09 11:05:02 +0800

    main

--- moyu_engine/config/setup.py
+++ moyu_engine/config/setup.py
@@ -25,9 +25,19 @@ window = pyglet.window.Window()
 window.set_caption('TinyLand 弹丸之地')
 #window.set_size(1280, 720)
 
+tl1 = pyglet.image.load('moyu_engine/assets/graphics/tileland/tl1.png')
+tl6 = pyglet.image.load('moyu_engine/assets/graphics/tileland/tl6.png')
+tl11 = pyglet.image.load('moyu_engine/assets/graphics/tileland/tl11.png')
+tl16 = pyglet.image.load('moyu_engine/assets/graphics/tileland/tl16.png')
+tl21 = pyglet.image.load('moyu_engine/assets/graphics/tileland/tl21.png')
+
 @window.event
 def on_draw():
     window.clear()
-    #label.draw()
+    tl1.blit(0, 50)
+    tl6.blit(50, 50)
+    tl11.blit(100, 50)
+    tl16.blit(150, 50)
+    tl21.blit(200, 50)
 
 pyglet.app.run()
